1f4b75e3a5f700137e53ff1aebe2e067825a92ec
Prajna Paramita Dash <Prajna Paramita Dash>
2017-04-25 14:40:25 -0400

    datasets changed to UKDale

--- predictive_modeling_part1/redd_fc.py
+++ predictive_modeling_part1/redd_fc.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('/home/amishra/appspace/Do-It-Yourslef-data-science/')
+sys.path.append(r'C:\Users\ppdash\workspace\Do-It-Yourslef-data-science')
 
 from common.data_server import BatchDataServer
 from common.h5_reader_writer import H5Writer
@@ -12,6 +12,7 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 import h5py
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 
 
@@ -38,16 +39,19 @@ class MnistModel:
         self.n_samples = None
         self.num_epochs = config['num_epochs']
         self.print_frequency = config['print_frequency']
+        self.feature_dimension = 400
+        self.number_class = 8
 
-        self.X = tf.placeholder(shape=(None, 400), dtype=tf.float32)
-        self.Y = tf.placeholder(shape=(None, 6), dtype=tf.float32)
+        self.X = tf.placeholder(shape=(None, self.feature_dimension), dtype=tf.float32)
+        self.Y = tf.placeholder(shape=(None, self.number_class), dtype=tf.float32)
         self.tensors = dict()
         self.model()
 
 
     def model(self):
-        self.tensors['w1'] = tf.Variable(tf.truncated_normal(shape=(400, 200), stddev=0.01))
+        self.tensors['w1'] = tf.Variable(tf.truncated_normal(shape=(self.feature_dimension, 200), stddev=0.01))
         self.tensors['b1'] = tf.Variable(tf.truncated_normal(shape =(1,200), stddev=0.01))
+        print(self.X.get_shape())
         self.tensors['h1'] = tf.nn.relu(tf.add(tf.matmul(self.X, self.tensors['w1']), self.tensors['b1']))
 
         self.tensors['w2'] = tf.Variable(tf.truncated_normal(shape=(200, 200), stddev=0.01))
@@ -59,11 +63,15 @@ class MnistModel:
         self.tensors['b3'] = tf.Variable(tf.truncated_normal(shape=(1, 100), stddev=0.01))
         self.tensors['h3'] = tf.nn.relu(tf.add(tf.matmul( self.tensors['h2'], self.tensors['w3']), self.tensors['b3']))
 
-        self.tensors['w4'] = tf.Variable(tf.truncated_normal(shape=(100, 6), stddev=0.01))
-        self.tensors['b4']= tf.Variable(tf.truncated_normal(shape=(1, 6), stddev=0.01))
+        self.tensors['w4'] = tf.Variable(tf.truncated_normal(shape=(100, 50), stddev=0.01))
+        self.tensors['b4'] = tf.Variable(tf.truncated_normal(shape=(1, 50), stddev=0.01))
+        self.tensors['h4'] = tf.nn.relu(tf.add(tf.matmul(self.tensors['h3'], self.tensors['w4']), self.tensors['b4']))
+
+        self.tensors['w5'] = tf.Variable(tf.truncated_normal(shape=(50, self.number_class), stddev=0.01))
+        self.tensors['b5']= tf.Variable(tf.truncated_normal(shape=(1, self.number_class), stddev=0.01))
 
 
-        self.tensors['y_hat'] = tf.nn.softmax(tf.add(tf.matmul(self.tensors['h3'], self.tensors['w4']), self.tensors['b4']))
+        self.tensors['y_hat'] = tf.nn.softmax(tf.add(tf.matmul(self.tensors['h4'], self.tensors['w5']), self.tensors['b5']))
         correct_prediction = tf.equal(tf.argmax(self.Y, 1), tf.argmax(self.predictor(), 1))
         self.tensors['accuracy'] = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
@@ -71,9 +79,12 @@ class MnistModel:
         return self.tensors['y_hat']
 
     def predict(self, learned_params, X=None):
+
         self.update_tensors_with_learned_params(learned_params)
+
         with tf.Session() as sess:
-            sess.run(tf.initialize_all_variables())
+            sess.run(tf.global_variables_initializer())
+
             return sess.run([self.predictor()],
                             feed_dict={self.X: X})
 
@@ -88,9 +99,15 @@ class MnistModel:
         self.tensors['h2'] = tf.nn.relu(tf.add(tf.matmul(self.tensors['h1'], self.tensors['w2']), self.tensors['b2']))
         self.tensors['w3'] = tf.Variable(learned_params['w3'])
         self.tensors['b3'] = tf.Variable(learned_params['b3'])
+        self.tensors['h3'] = tf.nn.relu(tf.add(tf.matmul(self.tensors['h2'], self.tensors['w3']), self.tensors['b3']))
+        self.tensors['w4'] = tf.Variable(learned_params['w4'])
+        self.tensors['b4'] = tf.Variable(learned_params['b4'])
+        self.tensors['h4'] = tf.nn.relu(tf.add(tf.matmul(self.tensors['h3'], self.tensors['w4']), self.tensors['b4']))
+        self.tensors['w5'] = tf.Variable(learned_params['w5'])
+        self.tensors['b5'] = tf.Variable(learned_params['b5'])
 
         self.tensors['y_hat'] = tf.nn.softmax(
-            tf.add(tf.matmul(self.tensors['h2'], self.tensors['w3']), self.tensors['b3']))
+            tf.add(tf.matmul(self.tensors['h4'], self.tensors['w5']), self.tensors['b5']))
 
     def cost_function(self):
         return tf.reduce_mean(-tf.reduce_sum( self.Y * tf.log(self.predictor()+ 1e-10), reduction_indices=[1]))
@@ -108,14 +125,15 @@ class MnistModel:
         return params_dict
 
     def confusion_mat(self, x, y):
-        res = tf.pack([tf.argmax(self.Y, 1), tf.argmax(self.predictor(), 1)])
+        res = tf.stack([tf.argmax(self.Y, 1), tf.argmax(self.predictor(), 1)])
         ans = res.eval(feed_dict={self.X: x, self.Y: y})
-        confusion = np.zeros([6, 6], int)
+        confusion = np.zeros([self.number_class, self.number_class], int)
         for p in ans.T:
             confusion[p[0], p[1]] += 1
 
         dataframe =  pd.DataFrame(confusion)
-        label = ['dish washer', 'fridge', 'light', 'washer dryer', 'microwave', 'sockets']
+        label = ['dw', 'fr', 'li', 'wd', 'mw', 'ket',
+                 'comp', 'oven']
         dataframe.index = label
         dataframe.columns = label
 
@@ -126,10 +144,13 @@ class MnistModel:
         H5Writer.write(filename, self.config, params_dict)
 
     def train(self, x=None, y=None, filename=None):
-        self.n_samples = 2476
-        train_X = x[0:self.n_samples, :]
-        train_Y = y[0:self.n_samples, :]
 
+        #self.n_samples = 2476
+
+        #train_X = x[0:self.n_samples, :]
+        #train_Y = y[0:self.n_samples, :]
+        train_X = x
+        train_Y = y
         test_X = train_X
         test_Y = train_Y
 
@@ -138,20 +159,25 @@ class MnistModel:
         solver = self.solver();
         batch_data = BatchDataServer(train_X, train_Y, batch_size = 128)
         lr = self.lr
+        loss = []
 
         with tf.Session() as sess:
-            sess.run(tf.initialize_all_variables())
+            sess.run(tf.global_variables_initializer())
+            #sess.run(tf.initialize_all_variables())
             [accuracy] = sess.run([self.tensors['accuracy']],
                                   feed_dict={self.X: train_X, self.Y: train_Y})
             self.confusion_mat(train_X, train_Y)
-            print("accuracy on testing images before training are", accuracy)
+            print("accuracy on testing appliance data before training are", accuracy)
             while batch_data.epoch < self.num_epochs:
                 x1, y1 = batch_data.next()
                 [_, cost] = sess.run([solver, cost_function],
                                          feed_dict={self.X: x1, self.Y: y1,  self.learning_rate:lr})
+                loss.append(cost)
+
                 if (batch_data.epoch + 1) % self.print_frequency == 0:
                     [cost] = sess.run([cost_function], feed_dict={self.X: train_X, self.Y: train_Y, self.learning_rate:lr})
                     print('At Epoch {} the loss is {}'.format(batch_data.epoch, cost))
+
                     batch_data.epoch = batch_data.epoch + 1
                     lr = self.lr * numpy.exp(-3.1*(batch_data.epoch/(self.num_epochs+1.0)))
                     print(lr)
@@ -159,47 +185,83 @@ class MnistModel:
                     [accuracy] = sess.run([self.tensors['accuracy']],
                                           feed_dict={self.X: test_X, self.Y: test_Y})
                     self.config['learning_rate'] = lr
-                    print("accuracy on testing images after training is {} with a learning rate of {}".format(accuracy, lr))
+                    print("accuracy on testing appliance data after training is {} with a learning rate of {}".format(accuracy, lr))
                     frame = self.confusion_mat(test_X, test_Y)
                     frame = frame / frame.sum()
                     if accuracy > 0.7:
                         print('writing to')
                         frame.to_csv('confusion.csv')
 
-
-        return params_dict
+        return_dict = {'parameters': params_dict, 'loss': loss}
+        return return_dict
 
 
 
 
 
 import numpy as np
+import random
 
-def test_train_mnist():
+def test_train_mnist(n_sample = None):
     config = {
-        'num_hidden_layers': 4,
+        'num_hidden_layers': 5,
         'order_poly': 4,
-        'learning_rate': 0.0001,
+        'learning_rate': 0.00001,
         'num_epochs': 10000,
-        'print_frequency': 100,
+        'print_frequency': 1000,
     }
-    f = h5py.File('/home/amishra/appspace/deep-nilmtk/common/b1to5top5.h5')
-    train_x = f['samples'].value
-    train_x = train_x / np.max(train_x) - 0.5
-    dummy = f['labels'].value
-    train_y = np.zeros(shape=(2476, 6))
-    for i in range(0, 2476):
-        train_y[i, dummy[i]] = 1
 
     lr = MnistModel(config)
-    learned_params =  lr.train(x = train_x, y = train_y, filename='/tmp/sin.h5')
-    #y_hat = lr.predict(learned_params, X=mnist.test.images[0:1, :])
-    #print(numpy.argmax(y_hat[0], axis=1))
+    f = h5py.File(r'C:\Users\ppdash\workspace\deep-nilmtk\Data\b1to5top5ukDale.h5')
+    x = f['samples'].value
 
+    n_sample = int(x.shape[0] * 0.8)
+    rIdx = random.sample(range(x.shape[0]), x.shape[0]);
+    print(rIdx)
+    x = x / np.max(x) - 0.5
+    x = x[rIdx]
+    train_x = x[0:n_sample,:]
+    test_x = x[n_sample:,:]
+    dummy = f['labels'].value
+    dummy = dummy[rIdx]
+    train_y = np.zeros(shape=(n_sample, lr.number_class))
+    for i in range(0, n_sample):
+        train_y[i, dummy[i]] = 1
+    test_dummy = dummy[n_sample:]
 
+    learned_params = lr.train(x=train_x, y=train_y, filename=None)
+    y_hat = lr.predict(learned_params['parameters'], X = test_x)
+    test_predict = np.argmax(y_hat[0], axis=1)
+    confusion_test = np.zeros([lr.number_class,lr.number_class], int)
 
+    for i in range(len(test_predict)):
+        confusion_test[test_dummy[i],test_predict[i]] += 1
+    dataframe_test = pd.DataFrame(confusion_test)
+    label = ['dw', 'fr', 'li', 'wd', 'mw', 'ket',
+                 'comp', 'oven']
+    dataframe_test.index = label
+    dataframe_test.columns = label
+    print('So test result is:')
+    print(pd.DataFrame(dataframe_test))
+    dataframe_test["sum"] = dataframe_test.sum(axis = 1)
+    dataframe_test_final = dataframe_test.loc[:,"dw":"oven"].div(dataframe_test["sum"], axis = 0)
+    print(np.round(dataframe_test_final,3))
+    true_match = 0;
+    # for i in range(len(test_predict)):
+    #     if test_dummy[i] == test_predict[i]:
+    #         true_match += 1
 
-
+    correct_prediction_test = np.float32(np.equal(test_dummy.reshape(-1,1),test_predict.reshape(-1,1)))
+    #print(correct_prediction_test)
+    accuracy_test = np.mean(correct_prediction_test)
+   # accuracy_test = true_match / len(test_predict)
+    print("accuracy on testing appliance data after training is {}".format(accuracy_test))
+    loss = learned_params['loss']
+    plt.plot(loss)
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.title('Loss in Training')
+    plt.show()
 
 if __name__ == '__main__':
     test_train_mnist()
